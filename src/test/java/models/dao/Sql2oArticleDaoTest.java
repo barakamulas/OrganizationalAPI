67d4056fa1587e88642c31d73819bf0902d38be0
barakamulas <barakafrank18@gmail.com>
2019-09-23 23:17:27 +0300

    Write tests for Sql2oAricleDao

--- src/test/java/models/dao/Sql2oArticleDaoTest.java
+++ src/test/java/models/dao/Sql2oArticleDaoTest.java
@@ -1,113 +1,113 @@
-//package models.dao;
+package models.dao;
+
+import models.Article;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+
+public class Sql2oArticleDaoTest {
+
+    private Sql2oArticleDao articleDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        articleDao = new Sql2oArticleDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void addingFoodSetsId() throws Exception {
+        Article testArticle = setupNewArticle();
+        int originalArticleId = testArticle.getId();
+        articleDao.add(testArticle);
+        assertNotEquals(originalArticleId, testArticle.getId());
+    }
+
+    @Test
+    public void addedArticlesAreReturnedFromGetAll() throws Exception {
+        Article testArticle = setupNewArticle();
+        articleDao.add(testArticle);
+        assertEquals(1, articleDao.getAll().size());
+    }
+
+    @Test
+    public void noArticlesReturnsEmptyList() throws Exception {
+        assertEquals(0, articleDao.getAll().size());
+    }
+
+    @Test
+    public void deleteByIdDeletesCorrectArticle() throws Exception {
+        Article article = setupNewArticle();
+        articleDao.add(article);
+        articleDao.deleteById(article.getId());
+        assertEquals(0, articleDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll() throws Exception {
+        Article testArticle = setupNewArticle();
+        Article otherArticle = setupNewArticle();
+        articleDao.clearAll();
+        assertEquals(0, articleDao.getAll().size());
+    }
+
+//    @Test
+//    public void addFoodTypeToRestaurantAddsTypeCorrectly() throws Exception {
 //
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.sql2o.Connection;
-//import org.sql2o.Sql2o;
+//        Restaurant testRestaurant = setupRestaurant();
+//        Restaurant altRestaurant = setupAltRestaurant();
 //
-//import static org.junit.Assert.*;
+//        restaurantDao.add(testRestaurant);
+//        restaurantDao.add(altRestaurant);
 //
-//public class Sql2oArticleDaoTest {
+//        Article testArticle = setupNewArticle();
 //
-//    private Sql2oFoodtypeDao foodtypeDao;
-//    private Connection conn;
+//        articleDao.add(testArticle);
 //
-//    @Before
-//    public void setUp() throws Exception {
-//        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
-//        Sql2o sql2o = new Sql2o(connectionString, "", "");
-//        restaurantDao = new Sql2oRestaurantDao(sql2o);
-//        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
-//        conn = sql2o.open();
-//    }
+//        articleDao.addArticleToRestaurant(testArticle, testRestaurant);
+//        articleDao.addArticleToRestaurant(testArticle, altRestaurant);
 //
-//    @After
-//    public void tearDown() throws Exception {
-//        conn.close();
+//        assertEquals(2, articleDao.getAllRestaurantsForAArticle(testArticle.getId()).size());
 //    }
 //
 //    @Test
-//    public void addingFoodSetsId() throws Exception {
-//        Foodtype testFoodtype = setupNewFoodtype();
-//        int originalFoodtypeId = testFoodtype.getId();
-//        foodtypeDao.add(testFoodtype);
-//        assertNotEquals(originalFoodtypeId, testFoodtype.getId());
+//    public void deleteingFoodTypeAlsoUpdatesJoinTable() throws Exception {
+//        Article testArticle  = setupNewArticle();
+//        articleDao.add(testArticle);
+//
+//        Article altTestArticle  = new Article("Seafood");
+//        articleDao.add(altTestArticle);
+//
+//        Restaurant testRestaurant = setupRestaurant();
+//        restaurantDao.add(testRestaurant);
+//
+//        Restaurant altRestaurant = setupAltRestaurant();
+//        restaurantDao.add(altRestaurant);
+//
+//        articleDao.addArticleToRestaurant(testArticle,testRestaurant);
+//        articleDao.addArticleToRestaurant(altTestArticle, altRestaurant);
+//
+//        articleDao.deleteById(testArticle.getId());
+//        assertEquals(0, articleDao.getAllRestaurantsForAArticle(testArticle.getId()).size());
 //    }
-//
-//    @Test
-//    public void addedFoodtypesAreReturnedFromGetAll() throws Exception {
-//        Foodtype testfoodtype = setupNewFoodtype();
-//        foodtypeDao.add(testfoodtype);
-//        assertEquals(1, foodtypeDao.getAll().size());
-//    }
-//
-//    @Test
-//    public void noFoodtypesReturnsEmptyList() throws Exception {
-//        assertEquals(0, foodtypeDao.getAll().size());
-//    }
-//
-//    @Test
-//    public void deleteByIdDeletesCorrectFoodtype() throws Exception {
-//        Foodtype foodtype = setupNewFoodtype();
-//        foodtypeDao.add(foodtype);
-//        foodtypeDao.deleteById(foodtype.getId());
-//        assertEquals(0, foodtypeDao.getAll().size());
-//    }
-//
-//    @Test
-//    public void clearAll() throws Exception {
-//        Foodtype testFoodtype = setupNewFoodtype();
-//        Foodtype otherFoodtype = setupNewFoodtype();
-//        foodtypeDao.clearAll();
-//        assertEquals(0, foodtypeDao.getAll().size());
-//    }
-//
-////    @Test
-////    public void addFoodTypeToRestaurantAddsTypeCorrectly() throws Exception {
-////
-////        Restaurant testRestaurant = setupRestaurant();
-////        Restaurant altRestaurant = setupAltRestaurant();
-////
-////        restaurantDao.add(testRestaurant);
-////        restaurantDao.add(altRestaurant);
-////
-////        Foodtype testFoodtype = setupNewFoodtype();
-////
-////        foodtypeDao.add(testFoodtype);
-////
-////        foodtypeDao.addFoodtypeToRestaurant(testFoodtype, testRestaurant);
-////        foodtypeDao.addFoodtypeToRestaurant(testFoodtype, altRestaurant);
-////
-////        assertEquals(2, foodtypeDao.getAllRestaurantsForAFoodtype(testFoodtype.getId()).size());
-////    }
-////
-////    @Test
-////    public void deleteingFoodTypeAlsoUpdatesJoinTable() throws Exception {
-////        Foodtype testFoodtype  = setupNewFoodtype();
-////        foodtypeDao.add(testFoodtype);
-////
-////        Foodtype altTestFoodtype  = new Foodtype("Seafood");
-////        foodtypeDao.add(altTestFoodtype);
-////
-////        Restaurant testRestaurant = setupRestaurant();
-////        restaurantDao.add(testRestaurant);
-////
-////        Restaurant altRestaurant = setupAltRestaurant();
-////        restaurantDao.add(altRestaurant);
-////
-////        foodtypeDao.addFoodtypeToRestaurant(testFoodtype,testRestaurant);
-////        foodtypeDao.addFoodtypeToRestaurant(altTestFoodtype, altRestaurant);
-////
-////        foodtypeDao.deleteById(testFoodtype.getId());
-////        assertEquals(0, foodtypeDao.getAllRestaurantsForAFoodtype(testFoodtype.getId()).size());
-////    }
-//
-//    // helpers
-//
-//    public Foodtype setupNewFoodtype() {
-//        return new Foodtype("Sushi");
-//    }
+
+    // helpers
+
+    public Article setupNewArticle() {
+        return new Article("Ballon D'or A Scam","In a controversial manner Messi pipped Van Dijk to win the Ballon D'or for the sixth time");
+    }
 //    public Restaurant setupAltRestaurant (){
 //        Restaurant restaurant = new Restaurant("Fish Omena", "214 NE Ngara", "97232", "254-402-9874");
 //        restaurantDao.add(restaurant);
@@ -119,6 +119,6 @@
 //        restaurantDao.add(restaurant);
 //        return restaurant;
 //    }
-//
-//
-//}
+
+
+}
