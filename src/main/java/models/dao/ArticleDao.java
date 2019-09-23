961f795b2b75d7ec3ba44f4fab2b5df4b04c6018
barakamulas <barakafrank18@gmail.com>
2019-09-23 23:10:16 +0300

    Create create.sql file which will create tables for the h2 database to be used

--- src/main/java/models/dao/ArticleDao.java
+++ src/main/java/models/dao/ArticleDao.java
@@ -1,4 +1,19 @@
 package models.dao;
 
+import models.Article;
+
+import java.util.List;
+
 public interface ArticleDao {
+
+
+    void add(Article article);
+
+    List<Article> getAll();
+
+    //update
+    //omit for now
+
+    void deleteById(int id);
+    void clearAll();
 }
---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,27 @@
+SET MODE PostgreSQL;
+
+CREATE TABLE IF NOT EXISTS users (
+ id int PRIMARY KEY auto_increment,
+ u_name VARCHAR,
+ role VARCHAR,
+ department_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+ id int PRIMARY KEY auto_increment,
+ title VARCHAR,
+ content VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS scoped_articles (
+ id int PRIMARY KEY auto_increment,
+ title VARCHAR,
+ content VARCHAR,
+ department_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS departments (
+ id int PRIMARY KEY auto_increment,
+ d_name VARCHAR,
+ no_of_employees INTEGER
+);
