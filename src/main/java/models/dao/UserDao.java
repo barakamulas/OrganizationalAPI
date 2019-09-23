f69be47a551bc25278ba82577e8ad2a1e31aea21
barakamulas <barakafrank18@gmail.com>
2019-09-23 23:49:01 +0300

    Implement methods defined in UserDao interface in Sql2oUserDao to ensure the tests pass

--- src/main/java/models/dao/UserDao.java
+++ src/main/java/models/dao/UserDao.java
@@ -1,4 +1,19 @@
 package models.dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+
+    void add(User article);
+
+    List<User> getAll();
+
+    //update
+    //omit for now
+
+    void deleteById(int id);
+    void clearAll();
 }
